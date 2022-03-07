37b760d89d822286f00c5359e38b2a051ba0cfdc
Surabhi Kumari <surabhi.kumari@city.ac.uk>
2022-03-07 23:02:02 +0000

    update single employee information

---
+++ main.py
@@ -0,0 +1,11 @@
+
+number_of_employees = input("Enter number of employees:")
+
+fname = input("Enter first name:")
+lname = input("Enter last name:")
+Age = input("Enter age of employee:")
+
+print("First name\t Last Name\t Age")
+
+print(f"{fname.capitalize()}  \t"+ f" {lname.capitalize()}  \t"+ f" {Age}  \t")
+
