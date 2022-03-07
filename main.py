92ff989121011055477180475a35c351cc2e4a7a
Surabhi Kumari <surabhi.kumari@city.ac.uk>
2022-03-07 23:58:59 +0000

    updated with array

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+fname_arr = []
+lname_arr = []
+Age_arr = []
 
-number_of_employees = input("Enter number of employees:")
+number_of_employees_str = input("Enter number of employees:")
+number_of_employee = int(number_of_employees_str)
 
-fname = input("Enter first name:")
-lname = input("Enter last name:")
-Age = input("Enter age of employee:")
+for index in range(number_of_employee):
+    fname = input("Enter first name:")
+    fname_arr.append(fname)
+    lname = input("Enter last name:")
+    lname_arr.append(lname)
+    Age_str = input("Enter age of employee:")
+    Age = int(Age_str)
+    Age_arr.append(Age)
 
 print("First name\t Last Name\t Age")
 
-print(f"{fname.capitalize()}  \t"+ f" {lname.capitalize()}  \t"+ f" {Age}  \t")
+for index in range(number_of_employee):
+    print(f"{fname_arr.pop(index).capitalize()}  \t"+ f" {lname_arr.pop(index).capitalize()}  \t"+ f" {Age_arr.pop(index)}  \t")
+
+
+
+
 
