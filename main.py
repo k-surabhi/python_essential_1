19671bd3477109316bdd1f4faef59fedfafdada8
Surabhi Kumari <surabhi.kumari@city.ac.uk>
2022-03-08 00:19:34 +0000

    updated with age logic between 18 and 100

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ fname_arr = []
 lname_arr = []
 Age_arr = []
 
+
+
 number_of_employees_str = input("Enter number of employees:")
 number_of_employee = int(number_of_employees_str)
 
 for index in range(number_of_employee):
+    x = None
     fname = input("Enter first name:")
     fname_arr.append(fname)
     lname = input("Enter last name:")
     lname_arr.append(lname)
-    Age_str = input("Enter age of employee:")
-    Age = int(Age_str)
-    Age_arr.append(Age)
+    while x == None:
+        Age_str = input("Enter age of employee:")
+        Age = int(Age_str)
+        if Age < 18 or Age > 100:
+            x = None
+        else:
+            x = True
+            Age_arr.append(Age)
 
 print("First name\t Last Name\t Age")
 
